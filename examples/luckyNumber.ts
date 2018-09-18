3ab1705dd7e04f6fc7ca6f9b02559d1a2451d1bd
Brendon <brendonferreiradm@gmail.com>
2018-09-18 00:26:04 -0300

    configured prettier on project

--- examples/luckyNumber.ts
+++ examples/luckyNumber.ts
@@ -1,37 +1,32 @@
-import Hippo from '../index';
+import Hippo from '../index'
 
 const vm = new Hippo({
-	variables: {
-		'lucky_number': 3
-	},
-	methods: {
-		'pick_random': ( ...args ) => args[ Math.floor(Math.random() * (args.length - 1)) + 0 ],
-		'is_between': ( value, min, max ) => value > min && value < max,
-		'if_else': ( condition, valueIfTrue, valueIfFalse ) => condition && valueIfTrue || valueIfFalse
-	}
+  variables: {
+    lucky_number: 3,
+  },
+  methods: {
+    pick_random: (...args) =>
+      args[Math.floor(Math.random() * (args.length - 1)) + 0],
+    is_between: (value, min, max) => value > min && value < max,
+    if_else: (condition, valueIfTrue, valueIfFalse) =>
+      (condition && valueIfTrue) || valueIfFalse,
+  },
 })
 
-vm.exec(
-	[ 
-		[ 3, 4, 5, 7, 2, 1, '@pick_random' ], 
-		'random_pick', 
-		'@set'
-	]
-)
+vm.exec([[3, 4, 5, 7, 2, 1, '@pick_random'], 'random_pick', '@set'])
 
-vm.exec(
-	[ 
-		[ '$random_pick', '$lucky_number', "@eq"],
-		'is_my_number', 
-		'@set'
-	] 
-)
+vm.exec([['$random_pick', '$lucky_number', '@eq'], 'is_my_number', '@set'])
 
-console.log( '$is_my_number:', vm.exec('$is_my_number') )
-console.log( '$random_pick:', vm.exec('$random_pick') )
-console.log( '$lucky_number:', vm.exec('$lucky_number') )
-console.log( 'vars dump', vm.dump() )
+console.log('$is_my_number:', vm.exec('$is_my_number'))
+console.log('$random_pick:', vm.exec('$random_pick'))
+console.log('$lucky_number:', vm.exec('$lucky_number'))
+console.log('vars dump', vm.dump())
 
-const result = vm.exec( [ '$is_my_number', 'You are lucky!', 'Não foi dessa vez', '@if_else' ] )
+const result = vm.exec([
+  '$is_my_number',
+  'You are lucky!',
+  'Não foi dessa vez',
+  '@if_else',
+])
 
-console.log(result) // Super position variable 
+console.log(result) // Super position variable
--- index.ts
+++ index.ts
@@ -1,175 +1,159 @@
-const NOT = (x) => !x
+const NOT = x => !x
 const EQUAL = (x, y) => x == y
 const NOT_EQUAL = (x, y) => NOT(EQUAL(x, y))
 const BIGGER_THAN = (x, y) => x > y
 const LOWER_THAN = (x, y) => x < y
 const OR = (x, y) => x || y
 const AND = (x, y) => x && y
-const BIGGER_OR_EQUAL_THAN = (x, y) => OR( BIGGER_THAN(x, y), EQUAL(x, y) )
-const LOWER_OR_EQUAL_THAN = (x, y) => OR( LOWER_THAN(x, y), EQUAL(x, y) )
-const GET = (x) => x
+const BIGGER_OR_EQUAL_THAN = (x, y) => OR(BIGGER_THAN(x, y), EQUAL(x, y))
+const LOWER_OR_EQUAL_THAN = (x, y) => OR(LOWER_THAN(x, y), EQUAL(x, y))
+const GET = x => x
 
 export interface HippoConfig {
-	methods?: any,
-	variables?: any,
-	variablePrefix?: string,
-	methodPrefix?: string,
-	variableResolver?: Function,
-	plugins?: [ ( ( config?: HippoConfig ) => void)? ]
+  methods?: any
+  variables?: any
+  variablePrefix?: string
+  methodPrefix?: string
+  variableResolver?: Function
+  plugins?: [((config?: HippoConfig) => void)?]
 }
 
 const DEFAULT_CONFIG: HippoConfig = {
-	methods: {
-		'eq': EQUAL,
-		'neq': NOT_EQUAL,
-		'not': NOT,
-		'bt': BIGGER_THAN,
-		'bte' : BIGGER_OR_EQUAL_THAN,
-		'lt' : LOWER_THAN,
-		'lte' : LOWER_OR_EQUAL_THAN,
-		'and' : AND,
-		'or': OR,
-		'get': GET
-	},
-	variables: {
-	},
-	variablePrefix: '$',
-	methodPrefix: '@'
+  methods: {
+    eq: EQUAL,
+    neq: NOT_EQUAL,
+    not: NOT,
+    bt: BIGGER_THAN,
+    bte: BIGGER_OR_EQUAL_THAN,
+    lt: LOWER_THAN,
+    lte: LOWER_OR_EQUAL_THAN,
+    and: AND,
+    or: OR,
+    get: GET,
+  },
+  variables: {},
+  variablePrefix: '$',
+  methodPrefix: '@',
 }
 
 export default class Hippo {
+  config: HippoConfig
+  _variableResolver: Function
+  _methods = {}
+  _variables = {}
 
-	config: HippoConfig
-	_variableResolver: Function
-	_methods = {}
-	_variables = {}
+  constructor(config?: HippoConfig) {
+    this._configure(config)
 
-	constructor(config?: HippoConfig) {
-		this._configure(config)
+    this.registerMethod('set', (value, name) => {
+      this.registerVariable(name, value)
+    })
+  }
 
-		this.registerMethod( 'set', (value, name) => {
-			this.registerVariable( name, value )
-		})
+  _configure(config: HippoConfig = {}) {
+    this.config = {
+      variablePrefix: config.variablePrefix || DEFAULT_CONFIG.variablePrefix,
+      methodPrefix: config.methodPrefix || DEFAULT_CONFIG.methodPrefix,
+      variables: {
+        ...DEFAULT_CONFIG.variables,
+        ...(config.variables || {}),
+      },
+      methods: {
+        ...DEFAULT_CONFIG.methods,
+        ...(config.methods || {}),
+      },
+      plugins: config.plugins || [],
+    }
 
-	}
-	
-	_configure(config: HippoConfig = {}) {
-		
-		this.config = { 
-			variablePrefix: config.variablePrefix || DEFAULT_CONFIG.variablePrefix,
-			methodPrefix: config.methodPrefix || DEFAULT_CONFIG.methodPrefix,
-			variables: {
-				...DEFAULT_CONFIG.variables,
-				...( config.variables || {} )
-			},
-			methods: {
-				...DEFAULT_CONFIG.methods,
-				...( config.methods || {} )
-			},
-			plugins: config.plugins || []
-		}
+    this._variableResolver = config.variableResolver || (x => x)
 
-		this._variableResolver = config.variableResolver || ( (x) => x )
+    this._methods = {}
+    for (const name in this.config.methods) {
+      this.registerMethod(name, this.config.methods[name])
+    }
 
-		this._methods = {}
-		for( const name in this.config.methods ) {
-			this.registerMethod( name, this.config.methods[name] )
-		}
-		
-		this._variables = {}
-		for( const name in this.config.variables ) {
-			this.registerVariable( name, this.config.variables[name] )
-		}
+    this._variables = {}
+    for (const name in this.config.variables) {
+      this.registerVariable(name, this.config.variables[name])
+    }
 
-		for( const plugin of this.config.plugins ) {
-			plugin( this.config )
-		}
+    for (const plugin of this.config.plugins) {
+      plugin(this.config)
+    }
+  }
 
-	}
+  registerMethod(name, fn) {
+    this._methods[this.config.methodPrefix + name] = fn
+  }
 
-	registerMethod(name, fn){
-	
-		this._methods[ this.config.methodPrefix + name ] = fn
-	}	
-	
-	executeMethod( name, args ) {
+  executeMethod(name, args) {
+    if (!this._methods[name]) {
+      throw new Error('Method not defined: ' + name)
+    }
 
-		if( !this._methods[name] ) {
-			throw new Error('Method not defined: '+ name)
-		}
+    return this._methods[name](...args)
+  }
 
-		return this._methods[ name ]( ...args )
-	}
-	
-	isMethod(fragment) {
-		
-		return fragment.charAt(0) === this.config.methodPrefix
-	}
-	
-	isVariable(fragment) {
+  isMethod(fragment) {
+    return fragment.charAt(0) === this.config.methodPrefix
+  }
 
-		return fragment.charAt(0) === this.config.variablePrefix
-	}
-	
-	registerVariable(name, value){
-		
-		this._variables[ this.config.variablePrefix + name ] = value
-	}
+  isVariable(fragment) {
+    return fragment.charAt(0) === this.config.variablePrefix
+  }
 
-	resolveVariable(name) {
-		
-		let result = this._variableResolver(name) 
-		if( name != result ) {
-			return result
-		}
+  registerVariable(name, value) {
+    this._variables[this.config.variablePrefix + name] = value
+  }
 
-		if( typeof this._variables[name] !== 'undefined' ) {
-			return this._variables[name]
-		}
+  resolveVariable(name) {
+    let result = this._variableResolver(name)
+    if (name != result) {
+      return result
+    }
 
-		throw new Error('Variable is not defined: ' + name)
-	}
-	
-	dump() {
-		return this._variables
-	}
+    if (typeof this._variables[name] !== 'undefined') {
+      return this._variables[name]
+    }
 
-	exec( expression ) {
-		
-		if( typeof expression === 'string' ) {
-			if( this.isVariable(expression) ) {
-				return this.resolveVariable(expression)
-			}
-		}
+    throw new Error('Variable is not defined: ' + name)
+  }
 
-		const stack = new Array()
+  dump() {
+    return this._variables
+  }
 
-		for( const fragment of expression ) {
-			let resolvedFragment = fragment
-			
-			if( fragment instanceof Array ) {
-				resolvedFragment = this.exec( fragment )
-			}
+  exec(expression) {
+    if (typeof expression === 'string') {
+      if (this.isVariable(expression)) {
+        return this.resolveVariable(expression)
+      }
+    }
 
-			if( typeof fragment === 'string' ) {
+    const stack = new Array()
 
-				if( this.isMethod(fragment) ) {
-					const args = Array.from(stack)
-					stack.splice(0, stack.length)
-					return this.executeMethod(fragment, args)
-				}
+    for (const fragment of expression) {
+      let resolvedFragment = fragment
 
-				if ( typeof fragment === 'string' ) {
-					if( this.isVariable(fragment) ) {
-						resolvedFragment = this.exec(fragment)
-					}
-				}
-			}
+      if (fragment instanceof Array) {
+        resolvedFragment = this.exec(fragment)
+      }
 
-			stack.push(resolvedFragment)
-		}
+      if (typeof fragment === 'string') {
+        if (this.isMethod(fragment)) {
+          const args = Array.from(stack)
+          stack.splice(0, stack.length)
+          return this.executeMethod(fragment, args)
+        }
 
-		
-	}
+        if (typeof fragment === 'string') {
+          if (this.isVariable(fragment)) {
+            resolvedFragment = this.exec(fragment)
+          }
+        }
+      }
 
+      stack.push(resolvedFragment)
+    }
+  }
 }
